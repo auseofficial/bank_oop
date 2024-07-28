9b14e1b3b35d8fdc6e68922683297d238cecd443
Akib Us Suny Eshan <97244965+auseofficial@users.noreply.github.com>
2024-07-28 09:21:57 +0600

    Update bank_oop.py

--- bank_oop.py
+++ bank_oop.py
@@ -24,33 +24,33 @@ acc1 = Bank(123456, "Akib Us Suny Eshan", 0)
 acc1.deposit(10)
 acc1.withdraw(500)
 
-class Bank:
-    def __init__(self, account_number, account_holder, initial_balance=0):
-        self.acc_num = account_number
-        self.acc_hol = account_holder
-        self.balance = initial_balance
+# class Bank:
+#     def __init__(self, account_number, account_holder, initial_balance=0):
+#         self.acc_num = account_number
+#         self.acc_hol = account_holder
+#         self.balance = initial_balance
 
-    def deposit(self, amount):
-        self.balance += amount
-        print(f"Deposited {amount}. Total balance: {self.get_balance()}")
+#     def deposit(self, amount):
+#         self.balance += amount
+#         print(f"Deposited {amount}. Total balance: {self.get_balance()}")
 
-    def withdraw(self, amount):
-        if amount > self.balance:
-            print("Insufficient balance!")
-        else:
-            self.balance -= amount
-            print(f"Withdrew {amount}. Total balance: {self.get_balance()}")
+#     def withdraw(self, amount):
+#         if amount > self.balance:
+#             print("Insufficient balance!")
+#         else:
+#             self.balance -= amount
+#             print(f"Withdrew {amount}. Total balance: {self.get_balance()}")
 
-    def get_balance(self):
-        return self.balance
+#     def get_balance(self):
+#         return self.balance
 
-    def __str__(self):
-        return (f"Account Number: {self.acc_num}, Account Holder: {self.acc_hol}, Balance: {self.balance}")
+#     def __str__(self):
+#         return (f"Account Number: {self.acc_num}, Account Holder: {self.acc_hol}, Balance: {self.balance}")
 
-acc1 = Bank(123456, "Akib Us Suny Eshan", 0)
-acc1.deposit(1000)
-acc1.withdraw(500)
-print(acc1)  
+# acc1 = Bank(123456, "Akib Us Suny Eshan", 0)
+# acc1.deposit(1000)
+# acc1.withdraw(500)
+# print(acc1)  
 
 
 
